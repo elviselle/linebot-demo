afe5f767eb89be9f290f68f78d7881f3d4872ba2
Elvis Wang <elviselle@gmail.com>
2025-04-15 02:26:32 -0400

    first init

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+
+app = Flask(__name__)
+
+# 從環境變數讀取憑證
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    incoming_msg = event.message.text
+    reply_msg = f'你說了：{incoming_msg}'
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_msg)
+    )
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
