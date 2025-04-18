55f6cd09ffd83bedea5407a23be91606140de04b
Elvis Wang <elviselle@gmail.com>
2025-04-18 12:13:45 -0400

    change booking message button size

--- app_v3.py
+++ app_v3.py
@@ -55,73 +55,190 @@ btn_message_template="""
             "spacing": "sm",
             "contents": [
               {
-                "type": "box",
-                "layout": "vertical",
-                "backgroundColor": "#FFAA88",
-                "cornerRadius": "md",
+                "type": "button",
                 "action": {
                   "type": "postback",
                   "label": "10:30",
                   "data": "action=book&date=04-23&time=10:30",
                   "displayText": "我要預約 4/23 10:30"
                 },
-                "paddingAll": "md",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "10:30",
-                    "align": "center",
-                    "weight": "bold",
-                    "size": "lg",
-                    "color": "#ffffff"
-                  }
-                ]
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
               },
               {
-                "type": "box",
-                "layout": "vertical",
-                "backgroundColor": "#FFAA88",
-                "cornerRadius": "md",
+                "type": "button",
                 "action": {
                   "type": "postback",
                   "label": "13:00",
                   "data": "action=book&date=04-23&time=13:00",
                   "displayText": "我要預約 4/23 13:00"
                 },
-                "paddingAll": "md",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "13:00",
-                    "align": "center",
-                    "weight": "bold",
-                    "size": "lg",
-                    "color": "#ffffff"
-                  }
-                ]
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
               },
               {
-                "type": "box",
-                "layout": "vertical",
-                "backgroundColor": "#FFAA88",
-                "cornerRadius": "md",
+                "type": "button",
                 "action": {
                   "type": "postback",
                   "label": "15:00",
                   "data": "action=book&date=04-23&time=15:00",
                   "displayText": "我要預約 4/23 15:00"
                 },
-                "paddingAll": "md",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "15:00",
-                    "align": "center",
-                    "weight": "bold",
-                    "size": "lg",
-                    "color": "#ffffff"
-                  }
-                ]
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://vos.line-scdn.net/bot-designer-template-images/event/cony-card.png",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "backgroundColor": "#FFF3E0"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "lg",
+        "contents": [
+          {
+            "type": "text",
+            "text": "預約 - 後天",
+            "weight": "bold",
+            "size": "xl",
+            "color": "#FF6F61"
+          },
+          {
+            "type": "text",
+            "text": "4/24 (星期四)",
+            "size": "md",
+            "color": "#666666",
+            "wrap": true
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "10:30",
+                  "data": "action=book&date=04-24&time=10:30",
+                  "displayText": "我要預約 4/24 10:30"
+                },
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "13:00",
+                  "data": "action=book&date=04-24&time=13:00",
+                  "displayText": "我要預約 4/24 13:00"
+                },
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "15:00",
+                  "data": "action=book&date=04-24&time=15:00",
+                  "displayText": "我要預約 4/24 15:00"
+                },
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://vos.line-scdn.net/bot-designer-template-images/event/sally-card.png",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "backgroundColor": "#FFE4E1"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "lg",
+        "contents": [
+          {
+            "type": "text",
+            "text": "預約 - 大後天",
+            "weight": "bold",
+            "size": "xl",
+            "color": "#FF6F61"
+          },
+          {
+            "type": "text",
+            "text": "4/25 (星期五)",
+            "size": "md",
+            "color": "#666666",
+            "wrap": true
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "10:30",
+                  "data": "action=book&date=04-25&time=10:30",
+                  "displayText": "我要預約 4/25 10:30"
+                },
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "13:00",
+                  "data": "action=book&date=04-25&time=13:00",
+                  "displayText": "我要預約 4/25 13:00"
+                },
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "15:00",
+                  "data": "action=book&date=04-25&time=15:00",
+                  "displayText": "我要預約 4/25 15:00"
+                },
+                "style": "primary",
+                "color": "#FFAA88",
+                "height": "md"
               }
             ]
           }
